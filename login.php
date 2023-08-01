5520a4d54b50dcdaf9dc217eb18c69ffa08a0b4a
S Abhimanyu <111636483+s-abhimanyu@users.noreply.github.com>
2023-08-01 19:11:33 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,83 @@
+<?php 
+        if ( !isset($_SESSION) ) session_start();
+
+        if(isset($_SESSION["user"]))
+        {
+            header("Location: customerhome.php");
+        }
+
+
+?>
+
+<html>
+<head>
+	
+
+<title>LOGIN</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>  
+<body>  
+
+
+		
+
+		
+		
+		<br><br>
+
+        
+
+		<div class="container">
+
+	    	<div class="d-flex justify-content-center align-items-center ">
+	    		<div class="row">
+	    			<div class="col-sm-4">
+	    				<h3><b>LOGIN</b></h3>
+	    			    <hr>
+	    			</div>
+	    		</div>
+	    		<div class="row">
+	    		
+	    			<div class="col-sm-4">
+                    <?php 
+
+                            if(isset($_SESSION["error"]))
+                            {
+                                echo "<p class=\"text-danger\">",$_SESSION["error"],"</p>" ;
+                                unset($_SESSION["error"]);
+                            }
+                            
+
+
+                    ?>
+
+	    				
+	    				<form action = "authentication.php"  method = "POST">
+							UserName  : <input autocomplete="off" class="form-control" type="text" name="user" required="">
+							<br><br>
+
+							Password  : <input autocomplete="off" class="form-control" type="password" name="pass" required="">
+							<br><br>
+							
+							<button class="btn btn-primary"><span class="glyphicon glyphicon-log-in"></span> Login</button>
+				        </form>
+						<br>
+						don't have an account? <a href="index.php">Signup</a>
+	    			</div>
+	    		
+	    	    </div>
+	    		
+	    	</div>
+    	
+        </div>
+
+       
+		
+
+
+
+	
+ 
+</body>  
+</html>  
